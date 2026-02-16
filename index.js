0efff7cc0c3e581b59c7c7b14843d81d5c9032fa
samatar mohamed <samatarmohamed59@gmail.com>
2026-02-16 08:10:08 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
 }
+function addingEventListener() {
+  const button = document.getElementById('button');
+
+  button.addEventListener('click', function() {
+    console.log("Button clicked");
+  });
+}
